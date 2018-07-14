8af2a031aa4b3e7a1dea1abbf670e20a58892cce
HigorLucas <higor.freitas@neppoadm.com.br>
2018-07-14 14:02:21 -0300

    Primeiro projeto

---
+++ src/main/java/com/springAprendendo/br/aprendendo/AprendendoApplication.java
@@ -0,0 +1,12 @@
+package com.springAprendendo.br.aprendendo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AprendendoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AprendendoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/springAprendendo/br/aprendendo/AprendendoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.springAprendendo.br.aprendendo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AprendendoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

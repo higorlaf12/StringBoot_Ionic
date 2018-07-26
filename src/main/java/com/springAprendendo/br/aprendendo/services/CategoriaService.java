55f6232e86b7edffb034d0fc356f19ec9547b8e0
HigorLucas <higor.freitas@neppoadm.com.br>
2018-07-26 13:53:24 -0300

    Atualizando categoria com PUT, outros pequenos ajustes

--- src/main/java/com/springAprendendo/br/aprendendo/services/CategoriaService.java
+++ src/main/java/com/springAprendendo/br/aprendendo/services/CategoriaService.java
@@ -14,13 +14,18 @@ public class CategoriaService {
     @Autowired
     private CategoriaRepository categoriaRepository;
 
-    public Categoria buscar(Integer id) {
+    public Categoria find(Integer id) {
         Optional<Categoria> obj = categoriaRepository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
     }
 
     public Categoria insert (Categoria categoria){
         categoria.setId(null);
+        return categoriaRepository.save(categoria);
+    }
+
+    public Categoria update(Categoria categoria){
+        find(categoria.getId());
         return categoriaRepository.save(categoria);
     }
 

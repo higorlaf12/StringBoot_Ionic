398a4f3e1e51a47425fb15f0d744f64e1961dbfe
HigorLucas <higor.freitas@neppoadm.com.br>
2018-07-27 17:16:04 -0300

    Validacao sintatica com Bean validation

--- src/main/java/com/springAprendendo/br/aprendendo/dto/CategoriaDTO.java
+++ src/main/java/com/springAprendendo/br/aprendendo/dto/CategoriaDTO.java
@@ -1,15 +1,18 @@
 package com.springAprendendo.br.aprendendo.dto;
 
 import com.springAprendendo.br.aprendendo.domain.Categoria;
+import org.hibernate.validator.constraints.Length;
 
+import javax.validation.constraints.NotEmpty;
 import java.io.Serializable;
 
 public class CategoriaDTO implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-
     private Integer id;
+    @NotEmpty(message = "Preenchimento obrigatorio")
+    @Length(min = 5, max = 80, message = "O tamanho deve ser entre 5 e 80 caracteres")
     private String nome;
 
     public CategoriaDTO() {

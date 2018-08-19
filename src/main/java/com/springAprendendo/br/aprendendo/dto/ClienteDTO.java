15123eceecae2464b429e855929dd77c4b9e3926
higorlaf12 <higor.freitas@neppoadm.com.br>
2018-08-19 18:54:36 -0300

    Validação customizada: e-mail não repetido na atualizacao de Cliente

--- src/main/java/com/springAprendendo/br/aprendendo/dto/ClienteDTO.java
+++ src/main/java/com/springAprendendo/br/aprendendo/dto/ClienteDTO.java
@@ -1,12 +1,14 @@
 package com.springAprendendo.br.aprendendo.dto;
 
 import com.springAprendendo.br.aprendendo.domain.Cliente;
+import com.springAprendendo.br.aprendendo.services.validation.ClienteUpdate;
 import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import java.io.Serializable;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 
     private static final long serialVersionUID = 1L;
---
+++ src/main/java/com/springAprendendo/br/aprendendo/services/validation/ClienteUpdateValidator.java
@@ -0,0 +1,48 @@
+package com.springAprendendo.br.aprendendo.services.validation;
+
+import com.springAprendendo.br.aprendendo.domain.Cliente;
+import com.springAprendendo.br.aprendendo.dto.ClienteDTO;
+import com.springAprendendo.br.aprendendo.repositories.ClienteRepository;
+import com.springAprendendo.br.aprendendo.resources.resources.execptions.FieldMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class ClienteUpdateValidator implements ConstraintValidator<ClienteUpdate, ClienteDTO> {
+    @Autowired
+    private HttpServletRequest request;
+
+    @Autowired
+    private ClienteRepository repo;
+
+    @Override
+    public void initialize(ClienteUpdate ann) {
+    }
+
+    @Override
+    public boolean isValid(ClienteDTO objDto, ConstraintValidatorContext context) {
+
+        @SuppressWarnings("unchecked")
+        Map<String, String> map = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+        Integer uriId = Integer.parseInt(map.get("id"));
+
+        List<FieldMessage> list = new ArrayList<>();
+
+        Cliente aux = repo.findByEmail(objDto.getEmail());
+        if (aux != null && !aux.getId().equals(uriId)) {
+            list.add(new FieldMessage("email", "Email já existente"));
+        }
+        for (FieldMessage e : list) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+                    .addConstraintViolation();
+        }
+        return list.isEmpty();
+    }
+}
